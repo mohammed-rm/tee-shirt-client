36f2e1a98d45bc50732915f46eb5cbe6b83d97b5
Mohammed R <mohammed.rmich@etu.univ-tours.fr>
2023-07-28 18:46:21 +0200

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {motion, AnimatePresence} from "framer-motion";
+import {useSnapshot} from "valtio";
+import {} from "../config/motion"
+
+const Home = () => {
+    return (<div>Home</div>);
+};
+
+export default Home;
