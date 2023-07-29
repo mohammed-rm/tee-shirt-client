a081b50ffc42b9a7a3cc28ded477506e76788a48
Mohammed R <mohammed.rmich@etu.univ-tours.fr>
2023-07-29 09:56:55 +0200

    tee-shirt 3D rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import Canvas from "./canvas/index.jsx";
+import CanvasModel from "./canvas/index.jsx";
 import Customizer from "./pages/Customizer.jsx";
 import Home from "./pages/Home.jsx";
 
 function App() {
     return (<main className="app transition-all ease-in bg-gradient-to-br from-blue-400 to-purple-500">
         <Home/>
-        <Canvas/>
+        <CanvasModel/>
         <Customizer/>
     </main>)
 }
