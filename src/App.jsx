eaec70c35b203ae578db5a705b59be9f7decc9ed
Mohammed R <mohammed.rmich@etu.univ-tours.fr>
2023-07-29 08:48:42 +0200

    left and bottom tabs

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Customizer from "./pages/Customizer.jsx";
 import Home from "./pages/Home.jsx";
 
 function App() {
-    return (<main className="app transition-all ease-in">
+    return (<main className="app transition-all ease-in bg-gradient-to-br from-blue-400 to-purple-500">
         <Home/>
         <Canvas/>
         <Customizer/>
--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -1,7 +1,58 @@
-import React from 'react';
+import React from "react";
+import {AnimatePresence, motion} from "framer-motion";
+import {useSnapshot} from "valtio";
+import state from "../store";
+import {EditorTabs, FilterTabs} from "../config/constants.js";
+import {fadeAnimation, slideAnimation} from "../config/motion.js";
+import {CustomButton, Tab} from "../components";
 
 const Customizer = () => {
-    return (<div>Customizer</div>);
+    const snap = useSnapshot(state);
+    return (<AnimatePresence>
+        {!snap.intro && (<>
+            <motion.div
+                key="custom"
+                className="absolute top-0 left-0 z-0"
+                {...slideAnimation("left")}
+            >
+                <div className="flex items-center min-h-screen">
+                    <div className="editortabs-container tabs">
+                        {EditorTabs.map((tab) => (<Tab
+                            key={tab.name}
+                            tab={tab}
+                            handleClick={() => {
+                            }}
+                        />))}
+                    </div>
+                </div>
+            </motion.div>
+            <motion.div
+                className="absolute z-10 top-5 right-5"
+                {...fadeAnimation}
+            >
+                <CustomButton
+                    type="filled"
+                    title="Go back"
+                    handleClick={() => state.intro = true}
+                    customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+                />
+            </motion.div>
+            <motion.div
+                className="filtertabs-container"
+                {...slideAnimation("up")}
+            >
+                {FilterTabs.map((tab) => (<Tab
+                    key={tab.name}
+                    tab={tab}
+                    isFilterTab
+                    isActive=""
+                    handleClick={() => {
+                    }}
+                />))}
+            </motion.div>
+        </>)}
+
+    </AnimatePresence>);
 };
 
 export default Customizer;
